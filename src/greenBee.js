a8ddf98e76ac59c881d54588ff9a227c4d27300a
Thomas Lyons <talyons22@gmail.com>
2020-01-11 11:31:35 -0800

    Create new dancer types

---
+++ src/greenBee.js
@@ -0,0 +1,36 @@
+var GreenDancer = function (top, left, timeBetweenSteps) {
+
+  MakeDancer.call(this, top, left, timeBetweenSteps);
+
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+
+  // var oldStep = this.step;
+
+  // this.step = function() {
+  //   // call the old version of step at the beginning of any call to this new version of step
+
+  //   oldStep();
+  //   // toggle() is a jQuery method to show/hide the <span> tag.
+  //   // See http://api.jquery.com/category/effects/ for this and
+  //   // other effects you can use on a jQuery-wrapped html tag.
+  //   this.$node.toggle();
+  // };
+
+  //return blinkyDancer;
+};
+
+GreenDancer.prototype = Object.create(MakeDancer.prototype);
+
+GreenDancer.prototype.step = function () {
+  // call the old version of step at the beginning of any call to this new version of step
+
+  // this.oldStep();
+  MakeDancer.prototype.step.call(this);
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  this.$node.toggle();
+};
+
+GreenDancer.prototype.constructor = MakeBlinkyDancer;
